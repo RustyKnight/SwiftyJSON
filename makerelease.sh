9c418acd8cdeb9ac4bf9a6a1d811b9adb0a9d6ee
Shane Whitehead <shane.whitehead@beamcommunications.com>
2017-06-29 08:18:43 +1000

    Remove examples Add carthage scripts

---
+++ makerelease.sh
@@ -0,0 +1,6 @@
+sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
+xcodebuild -version
+time carthage build --no-skip-current
+source makearchive.sh
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+xcodebuild -version
