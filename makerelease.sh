d01deb50a5658c3aa3269ebeca598a5ba11559b8
Shane Whitehead <shane.whitehead@beamcommunications.com>
2017-11-08 13:25:04 +1100

    Update scripts

--- makerelease.sh
+++ makerelease.sh
@@ -1,5 +1,3 @@
-sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
-xcodebuild -version
 rm *.zip
 rm -rf Carthage
 if [ "$1" == "-debug" ]; then
@@ -8,5 +6,3 @@ else
 	time carthage build --no-skip-current
 fi
 source makearchive.sh
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-xcodebuild -version
